024b0c5dabba5a76e60c65df5ad3a4813099664d
Mayur <mayurzope555@gmail.com>
2020-09-28 21:18:55 +0530

    [Mayur]Added: Email Part2 Email Validate mandatory pattern name

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,20 +1,20 @@
 /* Welcome to the Email Validation Problem */
+
 import java.util.*;
 
 public class EmailValidation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Email Validation Problem");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the email pattern to be validated");
 		String patternValidate = sc.next();
-		boolean matcher = patternValidate.matches("[A-Za-z0-9]+");
-
+		boolean matcher = patternValidate.matches("@[A-Za-z]+");
 
 		if(matcher == true) {
-			System.out.println(patternValidate+" Matches the Email Format");
+			System.out.println(patternValidate+" matches the Email Format");
 		}else {
-			System.out.println(patternValidate+" does not Match the Email Format");
+			System.out.println(patternValidate+" does not match.Invalid Format");
 		}
 
 	}
+
 }
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # EmailValidation
 
 Welcome to Email Validation Problem
+
+In Use Case 1 we were validating the mandatory pattern details required
+for the email validation
+
+In Use Case 2 we are validating the mandatory domain company name required
+for the email validation
