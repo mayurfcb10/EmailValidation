30ff7114f2105b56bd6835e5bc7c2da5fb2a27af
Mayur <mayurzope555@gmail.com>
2020-09-28 22:30:40 +0530

    [Mayur]Added: Email Validation by Merging all the use cases and validating input email pattern

--- EmailValidation.java
+++ EmailValidation.java
@@ -3,19 +3,18 @@
 import java.util.*;
 
 public class ValidateEmail {
-	public static void main(String[] args) {
+	public static void main(String[] args) public static void main(String[] args) {
 		System.out.println("Welcome to the Email Validation Problem");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the email pattern to be validated");
 		String patternValidate = sc.next();
-		boolean matcher = patternValidate.matches("^[._+-]+[A-Za-z]+");
+		boolean matcher = patternValidate.matches( "^[a-zA-Z0-9]{1,}[-+._][a-zA-Z0-9]{0,}@[a-zA-Z]{1,}.[a-zA-Z]{1,}.[a-zA-Z]*");
 
 		if(matcher == true) {
-			System.out.println(patternValidate+" matches the Email Format");
+			System.out.println("patternValidate"+" matches the Email Format");
 		}else {
-			System.out.println(patternValidate+" does not match the Format");
+			System.out.println("patternValidate"+" does not match the Format");
 		}
 
 	}
-
 }
--- README.md
+++ README.md
@@ -11,3 +11,6 @@ for the email validation.
 In Use Case 3 we are validating the pattern for (.com/.net) domains.  
 
 In Use Case 4 we are validating the pattern for optional part.
+
+In Use Case 5 File we are merging all the patterns and Validating the emails with the formats 
+available. 
